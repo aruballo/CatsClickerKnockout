660b132abf64171d7991c7027dc306de2a847760
Antonio <aruballo@uci.edu>
2015-05-06 21:32:09 -0700

    Changed level variable to a computed observable

--- js/CatsClicker.js
+++ js/CatsClicker.js
@@ -2,33 +2,39 @@ var catsClickerKnockOut = catsClickerKnockOut || {};
 
 catsClickerKnockOut.viewModel = {
 	init: function(){
-		this.catTitle = ko.observable('');
 		this.clickCount = ko.observable(0);
 		this.imgSrc = ko.observable("img/cat0.jpg");
+		this.catTitle = ko.observable("EL GATO SUPREME");
+		this.nicknames = ko.observableArray([
+			{nickname: "Torta"},
+			{nickname: "Paisa"},
+			{nickname: "Chapin"}
+		]);		
+		this.catLevel = ko.computed(function(){
+			clicks = catsClickerKnockOut.viewModel.clickCount();
+			if(clicks > 10 && clicks <= 20){
+				return "Newborn";
+			}
+			else if(clicks > 20 && clicks <= 30){
+				return "Infant";
+			}
+			else if(clicks > 30 && clicks <= 40){
+				return "Toddler";
+			}
+			else if(clicks > 40 && clicks <= 50){
+				return "Child";
+			}
+			else if(clicks > 50){
+				return "Teen";
+			}
+			else{
+				return "Fetus";
+			}
+		});
+		
 	},
 	updateClickCount: function(){
 		this.clickCount(this.clickCount() + 1);
-		this.updateTitle();
-	},
-	updateTitle: function(){
-		if(this.clickCount() > 10 && this.clickCount() <= 20){
-			this.catTitle("Newborn");
-		}
-		else if(this.clickCount() > 20 && this.clickCount() <= 30){
-			this.catTitle("Infant");
-		}
-		else if(this.clickCount() > 30 && this.clickCount() <= 40){
-			this.catTitle("Toddler");
-		}
-		else if(this.clickCount() > 40 && this.clickCount() <= 50){
-			this.catTitle("Child");
-		}
-		else if(this.clickCount() > 50){
-			this.catTitle("Teen");
-		}
-		else{
-			this.catTitle("Fetus");
-		}
 	}
 };
 
