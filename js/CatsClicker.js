e8127aa503caf13483fe243b059d6de805fd3c3c
Antonio <aruballo@uci.edu>
2015-05-07 23:05:22 -0700

    Added cat list that switches current cat

--- js/CatsClicker.js
+++ js/CatsClicker.js
@@ -1,6 +1,38 @@
 var catsClickerKnockOut = catsClickerKnockOut || {};
 
+catsClickerKnockOut.catsData = [
+	{
+			clickCount: 0,
+			imgSrc: "img/cat0.jpg",
+			catTitle: "EL GATO SUPREME",
+			nicknames: [{nickname: "Nacho"}, {nickname: "Gatito"}, {nickname: "El Rey"}]
+	},
+	{
+			clickCount: 0,
+			imgSrc: "img/cat1.jpg",
+			catTitle: "Amuricat",
+			nicknames: [{nickname: "Washington"}, {nickname: "Abraham"}, {nickname: "Eagle"}]
+	},
+	{
+			clickCount: 0,
+			imgSrc: "img/cat2.jpg",
+			catTitle: "Catonese",
+			nicknames: [{nickname: "Kawaii"}, {nickname: "Nani"}, {nickname: "Doshda"}]
+	},
+	{
+			clickCount: 0,
+			imgSrc: "img/cat3.jpg",
+			catTitle: "Vanilla cat",
+			nicknames: [{nickname: "Meow"}, {nickname: "Purr"}, {nickname: "Kitty"}]
+	},
+	{
+			clickCount: 0,
+			imgSrc: "img/cat4.jpg",
+			catTitle: "Canadicat",
+			nicknames: [{nickname: "Maple"}, {nickname: "Puck"}, {nickname: "Eh"}]
+	}	
 
+];
 catsClickerKnockOut.catObj = function(data){
 	this.clickCount = ko.observable(data.clickCount);
 	this.imgSrc = ko.observable(data.imgSrc);
@@ -31,17 +63,20 @@ catsClickerKnockOut.catObj = function(data){
 
 catsClickerKnockOut.viewModel = {
 	init: function(){
-		this.currentCat = ko.observable(new catsClickerKnockOut.catObj(
-			{
-				clickCount: 0,
-				imgSrc: "img/cat0.jpg",
-				catTitle: "EL GATO SUPREME",
-				nicknames: [{nickname: "Nacho"}, {nickname: "Gatito"}, {nickname: "Bob"}]
-			}
-		));
+		var self = this;
+		this.catList = ko.observableArray([]);
+
+		catsClickerKnockOut.catsData.forEach(function(cat){
+			self.catList().push(new catsClickerKnockOut.catObj(cat));
+		});
+		
+		this.currentCat = ko.observable(this.catList()[0]);
 	},
 	updateClickCount: function(){
 		this.clickCount(this.clickCount() + 1);
+	},
+	changeCat: function(){
+		catsClickerKnockOut.viewModel.currentCat(this);
 	}
 };
 
